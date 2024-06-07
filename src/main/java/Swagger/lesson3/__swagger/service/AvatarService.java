403a5c02423ad6338a1719225bcfa6aad51f84c0
elena_yagodkina <engineerlena@mail.ru>
2024-06-07 23:04:09 +0300

    lesson_3.5_fix

--- src/main/java/Swagger/lesson3/__swagger/service/AvatarService.java
+++ src/main/java/Swagger/lesson3/__swagger/service/AvatarService.java
@@ -9,6 +9,4 @@ public interface AvatarService {
     void uploadAvatar(Long studentId, MultipartFile file) throws IOException;
 
     Avatar findAvatar(Long studentId);
-
-    String getExtensions(String fileName);
 }
--- src/main/java/Swagger/lesson3/__swagger/service/AvatarServiceImpl.java
+++ src/main/java/Swagger/lesson3/__swagger/service/AvatarServiceImpl.java
@@ -4,16 +4,19 @@ import Swagger.lesson3.__swagger.model.Avatar;
 import Swagger.lesson3.__swagger.model.Student;
 import Swagger.lesson3.__swagger.repositories.AvatarRepository;
 import jakarta.transaction.Transactional;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
-
 @Service
 @Transactional
 public class AvatarServiceImpl implements AvatarService{
@@ -50,7 +53,26 @@ public class AvatarServiceImpl implements AvatarService{
         avatar.setFilePath(filePath.toString());
         avatar.setFileSize(file.getSize());
         avatar.setMediaType(file.getContentType());
+        avatar.setData(generateDataForDB(filePath));
         avatarRepository.save(avatar);
+    }
+
+    private byte[] generateDataForDB(Path filePath) throws IOException {
+        try (
+                InputStream is = Files.newInputStream(filePath);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                    BufferedImage image = ImageIO.read(bis);
+
+                    int height = image.getHeight() / (image.getWidth()/ 100);
+                    BufferedImage preview = new BufferedImage(100, height, image.getType());
+                    Graphics2D graphics2D = preview.createGraphics();
+                    graphics2D.drawImage(image, 0, 0, 100, height, null);
+                    graphics2D.dispose();
+
+                    ImageIO.write(preview, getExtensions(filePath.getFileName().toString()), baos);
+                    return baos.toByteArray();
+                }
     }
 
     @Override
@@ -58,7 +80,6 @@ public class AvatarServiceImpl implements AvatarService{
         return avatarRepository.findByStudentId(studentId).orElseThrow();
     }
 
-    @Override
     public String getExtensions(String fileName) {
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
